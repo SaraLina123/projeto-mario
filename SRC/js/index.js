908309076da8dd77a148a60cb2160af1df048360
Sara Sousa <100157402+SaraLina123@users.noreply.github.com>
2023-01-22 12:54:42 -0300

    Add files via upload

---
+++ SRC/js/index.js
@@ -0,0 +1,17 @@
+const botaoTrailer = documnet.querySelector('.botao-trailer');
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+botaoTrailer.addEventListener("click", () => {
+    modal.classList.toggle("aberto");
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.toggle("aberto");
+    video.setAttribute("src", "");
+});
+
+
